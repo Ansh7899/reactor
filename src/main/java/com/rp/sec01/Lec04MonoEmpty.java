792f4e133792d1a74135890b4e5d16f989263092
Ansh <vaguecoder145@gmail.com>
2023-12-29 19:32:51 +0530

    demo for mono.empty

---
+++ src/main/java/com/rp/sec01/Lec04MonoEmpty.java
@@ -0,0 +1,28 @@
+package com.rp.sec01;
+
+import com.rp.utils.Util;
+import reactor.core.publisher.Mono;
+
+// demonstrating the mono.empty() behaviour
+public class Lec04MonoEmpty {
+    public static void main(String[] args) {
+
+        userInfo(2).subscribe(
+                Util.onNext(),
+                Util.onError(),
+                Util.onComplete()
+        );
+
+    }
+
+    private static Mono<String> userInfo(int id){
+
+        if(id==1){
+            return Mono.just(Util.faker().name().firstName());
+        }else if(id == 2){
+            return Mono.empty();
+        } else
+            return Mono.error(new RuntimeException("not found"));
+
+    }
+}
