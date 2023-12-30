f4ace7520e76a9f93a8e4b54192a62d23a89a12c
Ansh <vaguecoder145@gmail.com>
2023-12-30 18:48:10 +0530

    Mono from runnable

---
+++ src/main/java/com/rp/sec01/Lec07MonoFromRunnable.java
@@ -0,0 +1,27 @@
+package com.rp.sec01;
+
+import com.rp.utils.Util;
+import reactor.core.publisher.Mono;
+
+public class Lec07MonoFromRunnable {
+    public static void main(String[] args) {
+
+        Mono.fromRunnable(timeTakingProcess()).subscribe(
+                Util.onNext(),
+                Util.onError(),
+                () -> {
+                    System.out.println("Process completed ... sending emails...");
+                }
+        );
+
+    }
+    private static Runnable timeTakingProcess() {
+        return () -> {
+
+            Util.sleepSeconds(4);
+            System.out.println("operation completed");
+
+        };
+    }
+
+}
