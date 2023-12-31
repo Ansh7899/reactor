3c4b255b2eb8adb57e02db4af063724d0e59681e
Ansh <vaguecoder145@gmail.com>
2023-12-31 12:11:56 +0530

    Create mono from flux

--- src/main/java/com/rp/Flux/Lec08FluxFromMono.java
+++ src/main/java/com/rp/Flux/Lec08FluxFromMono.java
@@ -16,6 +16,17 @@ public class Lec08FluxFromMono {
                 Util.onError(),
                 Util.onComplete()
         );
+//creating mono from flux
+        Flux.range(1,10)
+                .filter(i -> i>6)
+                .next()
+                .subscribe(Util.onNext(), Util.onError() , Util.onComplete());
 
+//        if we change position of filter, it will act as empty
+//        as first item we get is 1 which is not greater than 3
+        Flux.range(1,10)
+                .next()
+                .filter(i -> i>6)
+                .subscribe(Util.onNext(), Util.onError() , Util.onComplete());
     }
 }
