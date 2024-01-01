7db89eb99634e3c5e73a785242fdd4ae91fd5fce
Ansh <vaguecoder145@gmail.com>
2024-01-01 20:19:30 +0530

    switchIfEmpty operator

--- src/main/java/com/rp/Operators/Lec08SwitchIfEmpty.java
+++ src/main/java/com/rp/Operators/Lec08SwitchIfEmpty.java
@@ -3,24 +3,25 @@ package com.rp.Operators;
 import com.rp.utils.Util;
 import reactor.core.publisher.Flux;
 
-import java.time.Duration;
-
-//used to provide some default value if empty item comes
+//You can provide a fallback method instead of a default constant value
 //even if publisher publishes one item we won't invoke
-//defaultIfEmpty()
-public class Lec07DefaultIfEmpty {
+//switchIfEmpty()
+public class Lec08SwitchIfEmpty {
     public static void main(String[] args) {
 
         getNumbers()
                 .filter(i -> i>10)
-                .defaultIfEmpty(-1)
+                .switchIfEmpty(fallback())
                 .subscribe(Util.subscriber());
-
-
     }
 
     public static Flux<Integer> getNumbers (){
         return Flux.range(1, 10);
+
+    }
+
+    public static Flux<Integer> fallback (){
+        return Flux.range(20, 10);
 
     }
 }
